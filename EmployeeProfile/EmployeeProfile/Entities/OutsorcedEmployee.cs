1d74623eb9861ea8e394887154b0d6842302b008
Felipe Carvalho de Moura <felipe.moura@grupoverreschi.com.br>
2024-08-08 12:51:27 -0300

    new commit

---
+++ EmployeeProfile/EmployeeProfile/Entities/OutsorcedEmployee.cs
@@ -0,0 +1,20 @@
+﻿namespace EmployeeProfile.Entities
+{
+    internal class OutsorcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsorcedEmployee()
+        {
+        }
+        public OutsorcedEmployee(string name, int hours, double valuePerHour, double addtionalCharge) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = addtionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;
+         }
+    }
+}
