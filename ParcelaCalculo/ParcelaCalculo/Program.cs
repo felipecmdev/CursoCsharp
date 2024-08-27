a42713c33234817d7b6f71e143764582b07e6240
Felipe Carvalho de Moura <felipe.moura@grupoverreschi.com.br>
2024-08-27 16:34:49 -0300

    new commit

---
+++ ParcelaCalculo/ParcelaCalculo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ParcelaCalculo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter contract data");
+
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Date (dd/MM/yyyy): ");
+        }
+    }
+}
